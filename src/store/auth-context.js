79c0e800eed09d11c1a39441612e4e545e598074
Akuna444 <aklilsolomon3@gmail.com>
2023-01-03 16:44:15 +0300

    set: login persistance with local storage

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -5,31 +5,35 @@ const AuthContext = React.createContext({
   token: "",
   isLoggedIn: false,
   login: (token) => {},
-  logout: () => {}
-})
+  logout: () => {},
+});
 
-export function AuthContextProvider (props) {
-    const [token, setToken] = useState(null)
-    const isLoggedIn = !!token
+export function AuthContextProvider(props) {
+  const intialToken = localStorage.getItem("token");
+  const [token, setToken] = useState(intialToken);
+  const isLoggedIn = !!token;
 
-    const loginHandler = (token) => {
-        setToken(token)
-      
-    }
+  const loginHandler = (token) => {
+    setToken(token);
+    localStorage.setItem("token", token);
+  };
 
-    const logoutHandler = () => {
-        setToken(null)
-    }
+  const logoutHandler = () => {
+    setToken(null);
+    localStorage.removeItem("token");
+  };
 
-    const contextValue = {
-      token: token,
-      isLoggedIn: isLoggedIn,
-      login: loginHandler,
-      logout: logoutHandler
-    }
-    return (
-        <AuthContext.Provider value={contextValue}>{props.children}</AuthContext.Provider>
-    )
+  const contextValue = {
+    token: token,
+    isLoggedIn: isLoggedIn,
+    login: loginHandler,
+    logout: logoutHandler,
+  };
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {props.children}
+    </AuthContext.Provider>
+  );
 }
 
 export default AuthContext;
