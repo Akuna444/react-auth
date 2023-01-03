9272a5583051f53b98d70e212b7986fdd142c85d
Akuna444 <aklilsolomon3@gmail.com>
2023-01-03 13:43:31 +0300

    add change password functionality

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,41 @@
-import classes from './ProfileForm.module.css';
+import classes from "./ProfileForm.module.css";
+import { useRef, useContext } from "react";
+import AuthContext from "../../store/auth-context";
 
 const ProfileForm = () => {
+  const passwordInputRef = useRef();
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    const enteredPassword = passwordInputRef.current.value;
+    const authCtx = useContext(AuthContext)
+
+    fetch(,{
+      method:"POST",
+      body: JSON.stringify({
+        idToken: authCtx.token,
+        password: enteredPassword,
+        returnSecureInput: false 
+      }),
+      headers:{
+        "Context-Type" : "application/json"
+      }
+      
+    }).then(response => {
+      
+    });
+  };
   return (
-    <form className={classes.form}>
+    <form onSubmit={onSubmitHandler} className={classes.form}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input ref={passwordInputRef} type="password" id="new-password" />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
